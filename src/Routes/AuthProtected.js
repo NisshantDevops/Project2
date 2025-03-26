3ee1d7d9e641109a598ab28a9cf2e87a3d0be4b5
nishant <nishantjani942@gmail.com>
2025-03-26 18:01:33 +0530

    commit

--- src/Routes/AuthProtected.js
+++ src/Routes/AuthProtected.js
@@ -1,46 +1,16 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Navigate, Route } from "react-router-dom";
-import { setAuthorization } from "../helpers/api_helper";
-import { useDispatch } from "react-redux";
+const AuthProtected = ({ children }) => {
+  const token = localStorage.getItem("token");
 
-import { useProfile } from "../Components/Hooks/UserHooks";
-
-import { logoutUser } from "../slices/auth/login/thunk";
-
-const AuthProtected = (props) =>{
-  const dispatch = useDispatch();
-  const { userProfile, loading, token } = useProfile();
-  
-  useEffect(() => {
-    if (userProfile && !loading && token) {
-      setAuthorization(token);
-    } else if (!userProfile && loading && !token) {
-      dispatch(logoutUser());
-    }
-  }, [token, userProfile, loading, dispatch]);
-
-  /*
-    Navigate is un-auth access protected routes via url
-    */
-
-  if (!userProfile && loading && !token) {
-    return (
-      <Navigate to={{ pathname: "/login", state: { from: props.location } }} />
-    );
+  if (!token) {
+    return <Navigate to="/login" />;
   }
 
-  return <>{props.children}</>;
+  return <>{children}</>;
 };
 
 const AccessRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        return (<> <Component {...props} /> </>);
-      }}
-    />
-  );
+  return <Route {...rest} element={<Component />} />;
 };
-
 export { AuthProtected, AccessRoute };
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
-//Layouts
-import NonAuthLayout from "../Layouts/NonAuthLayout";
-import VerticalLayout from "../Layouts/index";
-
-//routes
+// routes
 import { authProtectedRoutes, publicRoutes } from "./allRoutes";
 import { AuthProtected } from './AuthProtected';
 
@@ -13,34 +9,27 @@ const Index = () => {
     return (
         <React.Fragment>
             <Routes>
-                <Route>
-                    {publicRoutes.map((route, idx) => (
-                        <Route
-                            path={route.path}
-                            element={
-                                <NonAuthLayout>
-                                    {route.component}
-                                </NonAuthLayout>
-                            }
-                            key={idx}
-                            exact={true}
-                        />
-                    ))}
-                </Route>
+                {/* Public Routes */}
+                {publicRoutes.map((route, idx) => (
+                    <Route
+                        path={route.path}
+                        element={<route.component />}
+                        key={idx}
+                    />
+                ))}
 
-                <Route>
-                    {authProtectedRoutes.map((route, idx) => (
-                        <Route
-                            path={route.path}
-                            element={
-                                <AuthProtected>
-                                    <VerticalLayout>{route.component}</VerticalLayout>
-                                </AuthProtected>}
-                            key={idx}
-                            exact={true}
-                        />
-                    ))}
-                </Route>
+                {/* Protected Routes */}
+                {authProtectedRoutes.map((route, idx) => (
+                    <Route
+                        path={route.path}
+                        element={
+                            <AuthProtected>
+                                <route.component />
+                            </AuthProtected>
+                        }
+                        key={idx}
+                    />
+                ))}
             </Routes>
         </React.Fragment>
     );
