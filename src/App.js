fce2c8bcaa07f26d18315eefe9f3856d1c211dab
nishant <nishantjani942@gmail.com>
2025-03-28 11:41:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import { ToastContainer } from "react-toastify";
 
 function App(){
   return (
+    <>
+          <ToastContainer 
+          position="top-right"
+          autoClose={3000}
+        />
     <React.Fragment>
     <ToastContainer position="top-right" autoClose={3000} />
       <Route/>
     </React.Fragment>
+    </>
   );
 }
 
